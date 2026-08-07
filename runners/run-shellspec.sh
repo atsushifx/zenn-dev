8946477a2ef0902bc5ba551b9793ffafbeac3004
Furukawa Atsushi <atsushifx@gmail.com>
2026-08-07 19:17:36 +0900

    style(runners): remove blank lines
    - runners/run-shellspec.sh:
      `set -euo pipefail` の直後の空行を削除
    - runners/run-shfmt.sh:
      ファイル末尾の空行を削除

--- runners/run-shellspec.sh
+++ runners/run-shellspec.sh
@@ -11,7 +11,6 @@
 
 set -euo pipefail
 
-
 # shellcheck source=runners/libs/init-vars.lib.sh
 . "$(dirname "${BASH_SOURCE[0]}")/libs/init-vars.lib.sh"
 
--- runners/run-shfmt.sh
+++ runners/run-shfmt.sh
@@ -48,4 +48,3 @@ main() {
 if [[ ${BASH_SOURCE[0]} == "$0" ]]; then
   main "$@"
 fi
-
